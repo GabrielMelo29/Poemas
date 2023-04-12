e34d8f28c83e30dd28a86aff0a0083e6351d44c2
GabrielMelo29 <gbrlmelo29@gmail.com>
2023-04-12 19:55:03 -0300

    animacao com animejs

--- script.js
+++ script.js
@@ -1,57 +1,108 @@
+const rioAudio = document.getElementById("rio");
+const animacao1 = document.querySelector(".modal-poema-1");
 
-// ------ iniciar animacao e audio rio raso
-const rioAudio = document.getElementById("rio"),
-animacao1 = document.querySelector(".modal-poema-1"),
-diminuir = document.querySelector("#diminuir"),
-aumentar = document.querySelector("#aumentar");
+
+var diminuirAnimation = anime({
+  targets: '#diminuir',
+  fontSize: 0,
+  duration: 5650,
+  easing: 'easeOutQuad',
+  autoplay: false,
+});
+var aumentarAnimation = anime({
+  targets: '#aumentar',
+  fontSize: [
+    { value: '0px', duration: 5, easing: 'easeOutExpo' },
+    { value: '90px', duration: 5650, easing: 'easeInQuad' }
+  ],
+  autoplay: false,
+});
+
 
 animacao1.addEventListener("mouseover", () => {
-    diminuir.classList.add('active')
-    aumentar.classList.add('active')
-    rioAudio.play();
 
+  diminuirAnimation.play();
+  aumentarAnimation.play();
+
+  rioAudio.play();
+  
 });
 animacao1.addEventListener("mouseout", () => {
-    diminuir.classList.remove('active') 
-    aumentar.classList.remove('active') 
-    rioAudio.load();
+
+  diminuirAnimation.restart();
+  diminuirAnimation.pause()
+
+  aumentarAnimation.restart();
+  aumentarAnimation.pause()
+
+  rioAudio.load();
+
 });
 
 
-// ------ iniciar animacao manso rio
-const animacao2 = document.querySelector(".modal-poema-2"),
-audio2 = document.getElementById("audio-2"),
-curve1 = document.querySelector(".curve-1"),
-curve2 = document.querySelector(".curve-2");
 
-var textElements = document.querySelectorAll(".text-fall");
+// -------------------------------- iniciar animacao manso rio
+
+const animacao2 = document.querySelector(".modal-poema-2");
+const audio2 = document.getElementById("audio-2");
+const curve1 = document.querySelector(".curve-1");
+const curve2 = document.querySelector(".curve-2");
+
+const textElements = document.querySelectorAll(".fall-animation span");
+var inicioAntecipado = 500;
+
+
+const sumir = anime.timeline();
+  sumir.add({
+    targets: textElements,
+    opacity: 0,
+    duration: 0001,
+  });
+  sumir.pause();
+
+
+const animation = anime.timeline();
+textElements.forEach((text, index) => {
+  animation.add({
+    delay: 3000,
+  });
+  animation.add({
+
+    targets: text,
+    opacity: 1,
+    easing: "easeInOutSine",
+    duration: 200,
+    delay: 700,
+    begin: () => {
+      audio2.currentTime = inicioAntecipado / 1000;
+      audio2.play();
+    },
+
+  });
+});
+animation.pause();
+
 
 animacao2.addEventListener("mouseover", () => {
+
     curve1.classList.add('active')
     curve2.classList.add('active')
 
-    fallAnimation();
-
+    sumir.play()
+    animation.play()
+    
 });
 animacao2.addEventListener("mouseout", () => {
+
     curve1.classList.remove('active')
     curve2.classList.remove('active')
+
+    sumir.restart()
+    sumir.pause()
+
+    animation.restart()
+    animation.pause()
+
     audio2.load();
 
 });
-
-
-
-index = 0;
-function fallAnimation() {
-    if(index < textElements.length) {
-        const span = textElements[index];
-        span.style.animationName = 'fall';
-        audio2.play()
-        console.log(span)
-        
-        index++;
-        setTimeout(fallAnimation, 2000);
-    }
-
-};
